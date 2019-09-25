450558ea0e2a8cf2f8c32c7eba44d9737975ad1a
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-25 20:59:33 +0100

    testing

--- src/com/jenkins/library/StructureTest.groovy
+++ src/com/jenkins/library/StructureTest.groovy
@@ -3,7 +3,8 @@ package com.jenkins.library
 
 def runTest(Map data=[:]) { 
     def structureTest = data.containerStructureTestPath
+    def image = data.dockerImage
     sh """ 
-        echo structure-test  
+        container-structure-test test --image ${image} -c ${structureTest}
     """
 }
