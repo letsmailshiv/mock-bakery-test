59119eae629ca8df67d8b576995a2fdd7e9d796d
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-25 22:05:30 +0100

    testing

--- src/com/jenkins/library/Connection.groovy
+++ src/com/jenkins/library/Connection.groovy
@@ -1,11 +1,14 @@
 package com.jenkins.library
 //This is to login onto registry to pull/push the images onto it.
 def login(Map config) {
-    def REGISTRYCREDS = config.registrycreds
+    def registrycreds = config.registrycreds
     def registry = config.registry
+    println("registrycreds = ${registrycreds}")
+    println("registrycreds = ${registry}")
     println "Connecting with Docker Registry..."
-    withCredentials([usernamePassword(credentialsId: "dev-robot", passwordVariable: 'registryPASS', usernameVariable: 'registryUSER')]) {
-        def status = sh(returnStatus: true, script: "docker login --username=${registryUSER} --password=${registryPASS} ${registry} > loginstatus.txt")
+ 
+    withCredentials([usernamePassword(credentialsId: "${registrycreds}", passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        def status = sh(returnStatus: true, script: "docker login --username=${USERNAME} --password=${PASSWORD} ${registry} > loginstatus.txt")
         if (status != 0) {
             println "Oops, The registry credentials you supplied are incorrect."
             currentBuild.result = 'FAILED'
