d2282d526a688a283f42b09a340c5491aaf664d4
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-26 00:01:57 +0100

    testing

--- vars/imageIngessionSuiteRTL.groovy
+++ vars/imageIngessionSuiteRTL.groovy
@@ -3,11 +3,11 @@ import com.jenkins.library.ImageIngessionSuite
 //import groovy.io.FileType
 
 def call(Map config=[:]) {
-    def yamlFile = config.yamlFile ? config.yamlFile : "${env.WORKSPACE}/pipelines/conf/imageIngessionRequestDEV.yaml"
+    def yamlFile = config.yamlFile ? config.yamlFile : "${env.WORKSPACE}/pipelines/conf/imageIngessionRequestRTL.yaml"
     Map yamlData = readYaml file: yamlFile
 
     ImageIngessionSuite imageingession = new ImageIngessionSuite();
     imageingession.ingessionSuite(yamlData)
-
+    
 }
 
