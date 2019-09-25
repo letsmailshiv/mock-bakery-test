1b71acfa4ff309a08a3335149eadda1fb932e89f
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-25 22:08:08 +0100

    testing

--- src/com/jenkins/library/ImagePush.groovy
+++ src/com/jenkins/library/ImagePush.groovy
@@ -4,6 +4,7 @@ def push(Map data=[:]) {
     println "Publishing the newly created docker image onto registry..."
     def registry = data.registry
     def registryProject = data.registryProject
+    def dockerImage = data.dockerImage
     sh """ 
         docker tag ${dockerImage} ${registry}/${registryProject}/${dockerImage}
         docker push ${registry}/${registryProject}/${dockerImage}
