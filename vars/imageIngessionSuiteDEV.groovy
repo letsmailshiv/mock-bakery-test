1b95d18558bb3c7079778d4b952ff29f57d746c7
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-25 16:14:06 +0100

    testing

--- vars/imageIngessionSuiteDEV.groovy
+++ vars/imageIngessionSuiteDEV.groovy
@@ -7,10 +7,9 @@ def call(Map config=[:]) {
 
     yamlData.images.each { data  -> 
         println("key =>" + data.key )
-        data.each { var ->         
-        println("dockerFileExists =>" + var )
-        println("dockerFileExists =>" + var.getClass() )
-        }
+        println("dockerFileExists =>" + data[data.key] )
+        println("dockerFileExists =>" + data[data.key].dockerFileExists )
+        println("dockerFileExists =>" + data[data.key].getClass() )
     }
     /*
     def foundYamlFiles = sh(script: "ls -1 ${env.WORKSPACE}/pipelines/conf/", returnStdout: true).split()
