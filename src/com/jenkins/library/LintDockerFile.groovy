913612b26544b7718ce6ed0a9b2093c0eef736e7
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-25 02:19:17 +0100

    testing

--- src/com/jenkins/library/LintDockerFile.groovy
+++ src/com/jenkins/library/LintDockerFile.groovy
@@ -1,5 +1,5 @@
 package com.jenkins.library
 //This is to build the docker image using buildah on container itself.
 def LintDockerFile(def dockerFilePath) {
-    sh "hadolint ${dockerFilePath}"
+    sh "hadolint ${pwd()}/${dockerFilePath}"
 }
