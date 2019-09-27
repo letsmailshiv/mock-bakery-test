bd1c00a833b920ee8091a93d37d01ede07f683c2
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-27 11:23:12 +0100

    testing

--- vars/imageIngestionSuiteDEV.groovy
+++ vars/imageIngestionSuiteDEV.groovy
@@ -3,7 +3,7 @@ import com.jenkins.library.ImageIngestionSuite
 //import groovy.io.FileType
 
 def call(Map config=[:]) {
-    def yamlFile = config.yamlFile ? config.yamlFile : "${env.WORKSPACE}/pipelines/conf/imageingestionRequestDEV.yaml"
+    def yamlFile = config.yamlFile ? config.yamlFile : "${env.WORKSPACE}/pipelines/conf/imageIngestionRequestDEV.yaml"
     Map yamlData = readYaml file: yamlFile
     yamlFile.path = yamlFile
     ImageIngestionSuite imageingestion = new ImageIngestionSuite();
--- vars/imageIngestionSuiteRTL.groovy
+++ vars/imageIngestionSuiteRTL.groovy
@@ -3,7 +3,7 @@ import com.jenkins.library.ImageIngestionSuite
 //import groovy.io.FileType
 
 def call(Map config=[:]) {
-    def yamlFile = config.yamlFile ? config.yamlFile : "${env.WORKSPACE}/pipelines/conf/imageingestionRequestRTL.yaml"
+    def yamlFile = config.yamlFile ? config.yamlFile : "${env.WORKSPACE}/pipelines/conf/imageIngestionRequestRTL.yaml"
     Map yamlData = readYaml file: yamlFile
 
     ImageIngestionSuite imageingestion = new ImageIngestionSuite();
