e2501fa47e4388402c50e4d925e7eb3c73922ad4
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-27 11:50:17 +0100

    testing

--- vars/imageIngestionSuiteRTL.groovy
+++ vars/imageIngestionSuiteRTL.groovy
@@ -5,7 +5,6 @@ import com.jenkins.library.ImageIngestionSuite
 def call(Map config=[:]) {
     def yamlFile = config.yamlFile ? config.yamlFile : "${env.WORKSPACE}/pipelines/conf/imageIngestionRequestRTL.yaml"
     Map yamlData = readYaml file: yamlFile
-    yamlFile.yamlPath = yamlFile
     ImageIngestionSuite imageingestion = new ImageIngestionSuite();
     imageingestion.ingestionSuite(yamlData,yamlFile)
     
