efe8663b4bcac5489794f5864da30b13a0970f1f
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-27 15:13:20 +0100

    testing

--- src/com/jenkins/library/PostIngestion.groovy
+++ src/com/jenkins/library/PostIngestion.groovy
@@ -20,5 +20,30 @@ def postProcess(Map data=[:],def yamlPath) {
     > ${yamlSource}
     """
     //Git Commit
-    sh "sleep 1500"
+    publishGitTag(credentialsId,gituser,gitemail){
+}
+
+
+def publishGitTag(credentialsId,gituser,gitemail){
+    //GIT tag
+    withCredentials([[$class: 'SSHUserPrivateKeyBinding', keyFileVariable: 'gitKey', passphraseVariable: 'gitKeyPass', credentialsId: credentialsId]]) {
+        def tagCommand = """
+                git config --global user.email \"${gitemail}\"
+                git config --global user.name \"${gituser}\"
+                git add . 
+                git commit -m \"Updating yaml\"; 
+                git push;
+            """
+        def cmd = """
+                chmod 600 ${env.gitKey}; 
+                eval `ssh-agent`; 
+                mkdir -p ~/.ssh
+                ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+                ssh-add ${env.gitKey};
+                ${tagCommand}
+                kill -s term \$SSH_AGENT_PID
+            """
+        out = sh(rturnStdout: true, script: cmd)
+    }
+
 }
