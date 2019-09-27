e35849b11a8b08c36c51d3359f17534ca467f7e9
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-27 12:02:06 +0100

    testing

--- src/com/jenkins/library/PostIngestion.groovy
+++ src/com/jenkins/library/PostIngestion.groovy
@@ -2,13 +2,13 @@ package com.jenkins.library
 //This is to build the docker image using buildah on container itself.
 def postProcess(Map data=[:],def yamlPath) {
     testYamlPath= "${data.containerStructureTestPath}"
-    yamlSource= "${data.path}"
+    yamlSource= "${yamlPath}"
+    yamlDest= "${pwd()}/images/DEV/ingessedImages.yaml"
     //Merge back change to master records.
-    //yamlMerge(
-    //    sourceFile: "${yamlSource}",
-    //    destFile: "${yamlDest}",
-    //)
-
+    yamlMerge(
+        sourceFile: "${yamlSource}",
+        destFile: "${yamlDest}",
+    )
     //Move testcase 
     sh  """
         mkdir -p ${pwd()}/images/DEV/tests/ && mv ${pwd()}/${testYamlPath} ${pwd()}/images/DEV/tests/
