beb81cb4937ea1e668b0aba9cbfc3f00fbe2bd62
Shivendra Sharma <shivendra.sharma@publicissapient.com>
2019-09-29 14:21:23 +0100

    testing

--- src/com/jenkins/library/BuildIt.groovy
+++ src/com/jenkins/library/BuildIt.groovy
@@ -2,7 +2,7 @@ package com.jenkins.library
 //This is to build the docker image using buildah on container itself.
 def buildit(def dockerFilePath) {
     sh """
-        INFO: Linting dockerfile
-        kaniko --dockerfile=${pwd()}/${dockerFilePath} --context=${pwd()}/pipelines/conf --no-push
+        echo INFO: Linting dockerfile
+        echo kaniko --dockerfile=${pwd()}/${dockerFilePath} --context=${pwd()}/pipelines/conf --no-push
     """    
 }
